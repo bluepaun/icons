529c443b0578bdede9fb2e2ead519694eee4b291
Jungwoo Cho <bluepaun@gmail.com>
2024-08-25 17:52:39 +0800

    add alert pop up

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const body = document.getElementsByTagName('body')[0];
+
 async function setCanvasImage(path) {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -35,12 +37,38 @@ Array(cnt).fill().forEach((_, idx) => {
         navigator.clipboard.write(clipboardItems)
           .then(() => console.log('copy success'))
           .catch(err => {
+              console.log (err);
+              let alert = document.getElementById('alert');
+              if (!alert) {
+                alert = document.createElement('div');
+                alert.id = 'alert';
+
+                const span = document.createElement('span');
+                alert.appendChild(span);
+
+                const close = document.createElement('button');
+                close.addEventListener('click', (e) => {
+                  const t = e.target;
+                  const p = document.getElementById('alert');
+                  body.removeChild(p);
+                });
+                close.innerText = 'close';
+                alert.appendChild(close);
+                
+                body.appendChild(alert);
+              }
+
+              const span = alert.getElementsByTagName('span')[0];
               if (err.name === 'NotAllowedError') {
                 // Permission denied
-                alert('Permission denied. Please allow access to the clipboard.');
+                //alert('Permission denied. Please allow access to the clipboard.');
+                span.innerText = 'Permission denied. Please allow access to the clipboard.';
               } else if (err.name === 'NotSupportedError') {
                 // Clipboard API not supported
-                alert('Clipboard API is not supported by your browser.');
+                //alert('Clipboard API is not supported by your browser.');
+                span.innerText = 'Clipboard API is not supported by your browser.';
+              } else {
+                span.innerText = 'Unknown err occured.';
               }
             });
       });
